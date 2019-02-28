4587c3dcd9fd082558401b31baadc5bc387c30ae
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-02-28 13:30:09 +0100

    Fixed CPU usage representation of the clients

--- experiment-1/client/base.js
+++ experiment-1/client/base.js
@@ -1,5 +1,6 @@
 const Influx = require('influx');
 const process = require('process');
+const osUtils = require('os-utils');
 const config = require('./config.json');
 
 class BaseClient {
@@ -11,6 +12,7 @@ class BaseClient {
                 {
                     measurement: 'usage_client',
                     fields: {
+                        cpuPercentage: Influx.FieldType.FLOAT,
                         cpuUser: Influx.FieldType.INTEGER,
                         cpuSystem: Influx.FieldType.INTEGER,
                         ramRss: Influx.FieldType.INTEGER,
@@ -57,24 +59,34 @@ class BaseClient {
      * @param event
      */
     saveUsage(event) {
-        this.influx.writePoints([
-            {
-                measurement: 'usage_client',
-                tags: {
-                    event: event
-                },
-                fields: {
-                    cpuUser: process.cpuUsage()['user'],
-                    cpuSystem: process.cpuUsage()['system'],
-                    ramRss: process.memoryUsage()['rss'],
-                    ramHeapTotal: process.memoryUsage()['heapTotal'],
-                    ramHeapUsed: process.memoryUsage()['heapUsed'],
-                    ramExternal: process.memoryUsage()['external']
-                },
-            }
-        ]).catch((err) => {
-            console.error(`Error while saving data to InfluxDB! ${err.stack}`)
-        })
+        let cpuUsagePercentage = 0.0;
+        osUtils.cpuUsage(((usage) => {
+            cpuUsagePercentage = usage;
+            console.info('EVENT: ' + event);
+            console.info('CPU (time ms): ' + JSON.stringify(process.cpuUsage()));
+	        console.info('CPU (%): ' + usage);
+            console.info('MEM: ' + JSON.stringify(process.memoryUsage()));
+
+            this.influx.writePoints([
+                {
+                    measurement: 'usage_client',
+                    tags: {
+                        event: event
+                    },
+                    fields: {
+                        cpuPercentage: cpuUsagePercentage,
+                        cpuUser: process.cpuUsage()['user'],
+                        cpuSystem: process.cpuUsage()['system'],
+                        ramRss: process.memoryUsage()['rss'],
+                        ramHeapTotal: process.memoryUsage()['heapTotal'],
+                        ramHeapUsed: process.memoryUsage()['heapUsed'],
+                        ramExternal: process.memoryUsage()['external']
+                    },
+                }
+            ]).catch((err) => {
+                console.error(`Error while saving data to InfluxDB! ${err.stack}`)
+            })
+        }).bind(this));
     }
 
     /**
