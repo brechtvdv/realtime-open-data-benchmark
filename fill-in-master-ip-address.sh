afbfdb0ed5db8fb80caf733914a82840587e1e4f
brechtvdv <br5cht@hotmail.com>
2019-05-20 14:21:57 +0200

    When localhost, update influxdb-datasource accordingly

--- fill-in-master-ip-address.sh
+++ fill-in-master-ip-address.sh
@@ -5,6 +5,7 @@ KUBE_MASTER_IP=$(kubectl cluster-info | head -n 1 | egrep '[0-9]+\.[0-9]+\.[0-9]
 if [ "$KUBE_MASTER_IP" == "" ]; then
 	# Remove externalIP config
 	sed --in-place "s/- EXTERNAL_IP//g" *.y*
+	sed --in-place "s/EXTERNAL_IP/localhost/g" influxdb-datasource.yml
 else 
 	echo "Kubernetes master's IP address is ${KUBE_MASTER_IP}"
 	sed --in-place "s/EXTERNAL_IP/${KUBE_MASTER_IP}/g" *.y*
